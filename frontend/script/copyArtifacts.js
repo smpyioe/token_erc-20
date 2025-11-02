4806501871d7ae5cfa2b64f4e4cb5b5b65e656eb
Smpyioe <smpyioe@gmail.com>
2025-11-02 15:22:59 +0100

    copy TokenFacotry.json in copyArtifact.js

--- frontend/script/copyArtifacts.js
+++ frontend/script/copyArtifacts.js
@@ -2,15 +2,28 @@
 import fs from "fs";
 import path from "path";
 
-const source = path.resolve("../backend/out/Token.sol/Token.json");
-console.log(source);
-const destination = path.resolve("../frontend/src/artifacts/Token.json");
-console.log(destination);
-try {
-  fs.mkdirSync(path.dirname(destination), { recursive: true });
-  fs.copyFileSync(source, destination);
-  console.log(" Token.json copied to frontend/src/artifacts/");
-} catch (error) {
-  console.error(" Error copying artifact:", error);
-  process.exit(1);
+
+
+function copyArtifacts(source,destination){
+  console.log(destination);
+  console.log(source);
+  try {
+    fs.mkdirSync(path.dirname(destination), { recursive: true });
+    fs.copyFileSync(source, destination);
+    console.log(" Token.json copied to frontend/src/artifacts/");
+  } catch (error) {
+    console.error(" Error copying artifact:", error);
+    process.exit(1);
+  }
 }
+
+
+const sourcetoken = path.resolve("../backend/out/Token.sol/Token.json");
+const destinationtoken = path.resolve("../frontend/src/artifacts/Token.json");
+copyArtifacts(sourcetoken,destinationtoken);
+
+
+
+const sourcetokenFactory = path.resolve("../backend/out/TokenFactory.sol/TokenFactory.json");
+const destinationtokenFactory = path.resolve("../frontend/src/artifacts/TokenFactory.json");
+copyArtifacts(sourcetokenFactory,destinationtokenFactory);
